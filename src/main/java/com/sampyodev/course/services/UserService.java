896693b8155b76941e5386d8c5fee640b986b173
Igor Sampaio <igorsampaio9103@gmail.com>
2024-03-26 16:09:37 -0300

    Exception handling - delete

--- src/main/java/com/sampyodev/course/services/UserService.java
+++ src/main/java/com/sampyodev/course/services/UserService.java
@@ -1,13 +1,17 @@
 package com.sampyodev.course.services;
 
 import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.sampyodev.course.entities.User;
 import com.sampyodev.course.repositories.UserRepository;
+import com.sampyodev.course.services.exceptions.DatabaseException;
 import com.sampyodev.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +34,13 @@ public class UserService {
 	}
 	
 	public void delet(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);			
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
